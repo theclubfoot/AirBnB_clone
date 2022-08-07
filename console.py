6d478f2023a2d0398d4b2ff9baddad6bb56ecde4
Seyi <kuforiji98@gmail.com>
2022-08-07 18:42:31 +0100

    pycodestyle fix

--- console.py
+++ console.py
@@ -146,12 +146,12 @@ class HBNBCommand(cmd.Cmd):
             if words[0] not in storage.classes():
                 print("** class doesn't exist **")
             else:
-                l = [str(obj) for key, obj in storage.all().items()
+                h = [str(obj) for key, obj in storage.all().items()
                      if type(obj).__name__ == words[0]]
-                print(l)
+                print(h)
         else:
-            l = [str(obj) for key, obj in storage.all().items()]
-            print(l)
+            h = [str(obj) for key, obj in storage.all().items()]
+            print(h)
 
     def do_count(self, line):
         """Counts the instances of a class.
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -76,8 +76,8 @@ class TestBaseModel(unittest.TestCase):
     def test_3_id(self):
         """Tests for unique user ids."""
 
-        l = [BaseModel().id for i in range(1000)]
-        self.assertEqual(len(set(l)), len(l))
+        h = [BaseModel().id for i in range(1000)]
+        self.assertEqual(len(set(h)), len(h))
 
     def test_3_save(self):
         """Tests the public instance method save()."""
